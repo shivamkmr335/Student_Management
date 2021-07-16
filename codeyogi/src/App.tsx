df185cae19561331c4d825ca9eaa7dbf7d06dbcc
shivamkmr335 <shivam.rai_cs18@gla.ac.in>
2021-07-16 08:03:28 +0530

    installed tailwind css and its dependencies

---
+++ codeyogi/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="h-10 bg-gray-400"> This is a Heading</div>
+    </div>
+  );
+}
+
+export default App;

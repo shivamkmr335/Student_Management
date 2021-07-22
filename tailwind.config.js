9c3d1cdebc41c7848b65f8f4a7f71b76dfdef206
shivamkmr335 <shivam.rai_cs18@gla.ac.in>
2021-07-22 15:47:15 +0530

    tailwind config color removed

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,7 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      colors:{
-        darkblue: "#0e1726",
-      }
-    },
+    
   },
   variants: {
     extend: {},

241e598a81eb030d5df089e5b8801a2cb6b17b6e
shivamkmr335 <shivam.rai_cs18@gla.ac.in>
2021-07-28 03:10:31 +0530

    lazy loading done

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,45 @@
-import React from 'react';
+import { FC , lazy , memo, Suspense} from 'react';
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import { LS_LOGIN_TOKEN } from './api/base';
-import AppContainerPage from './pages/AppContainer.page';
-import AuthPage from './pages/Auth.page';
 import NotFoundPage from './pages/NotFound.page';
 
-function App() {
+const AppContainerPageLazy = lazy(()=> import("./pages/AppContainer.page"));
+const AuthPageLazy = lazy(()=> import("./pages/Auth.page"));
+
+
+interface Props {
+}
+
+const App: FC<Props> = (props) => {
+
   const token=localStorage.getItem(LS_LOGIN_TOKEN);
 
   return (
-    <BrowserRouter>
+    <Suspense fallback={
+      <div className="text-red-500 ">Loading Page...</div>
+    }>
+      <BrowserRouter>
       <Switch>
         <Route path="/" exact>
           {token ? <Redirect to="/dashboard"></Redirect> : <Redirect to="/login"></Redirect> }
         </Route>
         <Route path={["/login","/signup"]}>
-        {token ? <Redirect to="/dashboard"></Redirect> : <AuthPage/> }
+        {token ? <Redirect to="/dashboard"></Redirect> : <AuthPageLazy/> }
         </Route> 
         <Route path={["/dashboard","/recordings","/lecture/:lectureNumber"]}>
-          {token ? <AppContainerPage/> : <Redirect to="/login"></Redirect> }
+          {token ? <AppContainerPageLazy/> : <Redirect to="/login"></Redirect> }
         </Route>
         <Route>
           <NotFoundPage></NotFoundPage>
         </Route>
       </Switch>
     </BrowserRouter>
+  </Suspense>
   );
+};
+
+App.defaultProps = {
 }
 
-export default App;
+export default  memo(App);
+

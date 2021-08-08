415aade6a4fa94b00a2b7ee88774a83e2588f8fb
shivamkmr335 <shivam.rai_cs18@gla.ac.in>
2021-08-09 03:25:35 +0530

    updated user model

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,9 +1,22 @@
 
 
 export interface User{
-    id: number;
-    first_name: string;
-    middle_name: string;
-    last_name: string;
-    role: "staff" | "admin";
+    first_name?: string;
+    middle_name?: string;
+    last_name?: string;
+    profile_pic_url?: string;
+    phone_number?: string;
+    alternate_phone_number?: string;
+    email?: string;
+    gender?: "male" | "female";
+    birth_year?: string;
+    birth_month?: string;
+    birth_date?: string;
+    death_year?: string;
+    death_month?: string;
+    death_date?: string;
+    party?: string;
+    home_state_code?: string;
+    education?: string;
+    hometown?: string;
 }
